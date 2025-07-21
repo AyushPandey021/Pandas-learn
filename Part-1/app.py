63b7b8ff1a5f91fb59f8cf660cff99cc0dae7c56
Ayush Pandey <ayushpandey4480@gmail.com>
2025-07-21 16:58:06 +0530

    manages the files by name to better understanding

--- Part-1/app.py
+++ Part-1/app.py
@@ -1,15 +1,15 @@
 import pandas as pd
+
 data = {
-  "Name":["Ram", 'Sita',"krishna"],
-  "age":[10,20,30],
-  "city":["Nagput", "hyderabad","delhi"]
- }
+    "Name": ["Ram", "Sita", "krishna"],
+    "age": [10, 20, 30],
+    "city": ["Nagput", "hyderabad", "delhi"],
+}
 df = pd.DataFrame(data)
 print(df)
 
 
-# df.to_csv("output.csv", index=False) #save in CSV file 
-# df.to_excel("output.xlsx", index=False) #save in Excel file 
+# df.to_csv("output.csv", index=False) #save in CSV file⏭️
+# df.to_excel("output.xlsx", index=False) #save in Excel file⏭️
 
-df.to_json("output.json", index=False) #save in json file  
-
+df.to_json("output.json", index=False)  # save in json file
